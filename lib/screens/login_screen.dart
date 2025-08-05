2d23cb21e729a690092a2291329ba3c044ed1124
DevfoxJeongHoeJun <jeonghj@devfox.co.jp>
2025-08-05 18:11:27 +0900

    mockup update

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'infoList.dart';
+import 'attendance_record_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+
+class LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isChecked = false;
+
+  void _login() {
+    final email = emailController.text;
+    final password = passwordController.text;
+    final masterPwd='1234';
+    final master='marster@devfox.jp';
+    if (email == master && password == masterPwd) {
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const Infolist()),
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const AttendanceRecordScreen()),
+      );
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        // 縦に配置
+        child: Column(
+          // 真ん中に
+          mainAxisAlignment: MainAxisAlignment.center,
+          // 子ウィジェット配置
+          children: [
+            // Text配置
+            const Text(
+              'ログイン',
+              // style適用
+              style: TextStyle(
+                fontSize: 40,
+                // 文字を厚く
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            // 空間生成
+            const SizedBox(height: 90),
+            // 特定位置に整列
+            Align(
+              // 左に整列
+              alignment: Alignment.centerLeft,
+              // 左に整列
+              child: const Text(
+                'メールアドレス',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            Align(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'パスワード',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Row(
+
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    '自動でログインチェック',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Checkbox(
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: _login,
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              child: const Text(
+                'ログイン',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/user_create_screen.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '社員登録',
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar( //タイトル
+            centerTitle: true,
+            title: Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Text("社員登録" //画面タイトル
+                ,style: TextStyle(
+                    fontSize: 30, //font size
+                    fontWeight: FontWeight.bold
+                ),
+              ),)
+        ),
+        //タイトル end--------------------------------
+
+        //ボディ start--------------------------------
+        body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Column( //テキストボックス パート
+                children: [
+                  //会社名start--------------------------------
+                  companyNameText(),
+                  textBox(),
+                  //会社名end--------------------------------
+
+                  //メールアドレスstart--------------------------------
+                  mailAddressText(),
+                  textBox(),
+                  //メールアドレスend--------------------------------
+
+                  //パスワードstart--------------------------------
+                  passwordText(),
+                  passwordTextBox(),
+                  //パスワードend--------------------------------
+
+                  //パスワード確認start--------------------------------
+                  passwordReText(),
+                  passwordTextBox(),
+                  //パスワード確認end--------------------------------
+
+                  //登録ボタンstart--------------------------------
+                  registButton(),
+                  //登録ボタンend--------------------------------
+                ].toList(),
+              ),
+            )),
+          ),
+          //ボディ end--------------------------------
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+//会社名テキスト
+class companyNameText extends StatelessWidget {
+  const companyNameText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      width: 800,
+      height: 35,
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 20,bottom: 5),
+      child: Text(
+        "会社名",//テキストボックスタイトル
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ), textAlign: TextAlign.left,
+      ),
+    );
+  }
+}
+
+//メールアドレステキスト
+class mailAddressText extends StatelessWidget {
+  const mailAddressText({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 800,
+      height: 35,
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 20,bottom: 5),
+      child:   Text(
+        "メールアドレス",//Company Location
+        style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+}
+
+//パスワードテキスト
+class passwordText extends StatelessWidget {
+  const passwordText({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 800,
+      height: 35,
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 20,bottom: 5),
+      child:   Text(
+        "パスワード",//Company Location
+        style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+}
+
+//パスワード確認テキスト
+class passwordReText extends StatelessWidget {
+  const passwordReText({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 800,
+      height: 35,
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 20,bottom: 5),
+      child:   Text(
+        "パスワード確認",//Company Location
+        style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+}
+
+//一般テキストボックス
+class textBox extends StatelessWidget {
+  const textBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 800,
+      height: 45,
+      child: TextField( //CompanyName
+        decoration: InputDecoration(
+            hintText: "テキストボックス",
+            filled: true,
+            fillColor: Colors.grey[100],
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(color: Colors.black)
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(color: Colors.black)
+            )
+        ),
+      ),
+    );
+  }
+}
+
+//パスワードテキストボックス
+class passwordTextBox extends StatelessWidget {
+  const passwordTextBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 800,
+      height: 45,
+      child: TextField( //CompanyName
+        obscureText: true,
+        decoration: InputDecoration(
+            hintText: "テキストボックス",
+            filled: true,
+            fillColor: Colors.grey[100],
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(color: Colors.black)
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(color: Colors.black)
+            )
+        ),
+      ),
+    );
+  }
+}
+
+//登録ボタン
+class registButton extends StatelessWidget {
+  const registButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 800,
+        height: 70,
+        margin: EdgeInsets.only(top: 50,bottom: 5),
+        child: ElevatedButton(onPressed: (){
+
+        },
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            backgroundColor: Colors.blueAccent,
+            foregroundColor: Colors.white,
+          ), child: Text("登録",style: TextStyle(fontSize: 40.0),),
+        )
+    );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var results = "";
+  final myController = TextEditingController(); //テキストボックスからデータを出す
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: myController,
+              decoration: InputDecoration(
+                  hintText: "Results"
+              ),
+            ),
+            registButton(
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
