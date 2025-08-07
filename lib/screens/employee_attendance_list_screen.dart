fa30158004f323e430692f79be6b30a84619d5e2
ハンサンヒョン <hansh@devfox.co.jp>
2025-08-07 11:21:02 +0900

    (ハン)社員勤怠一覧レイアウト修正インプットのプレイスホルダー修正

--- lib/screens/employee_attendance_list_screen.dart
+++ lib/screens/employee_attendance_list_screen.dart
@@ -203,7 +203,7 @@ class NameSearchInput extends StatelessWidget {
             ),
           ),
         ),
-        const SizedBox(width: 8),
+        const SizedBox(width: 18),
 
         SizedBox(
           height: 48,
