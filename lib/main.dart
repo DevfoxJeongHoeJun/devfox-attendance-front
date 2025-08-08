b8b3cad2a7f0aa74dcf44cf32a3522bae85d0b62
ハンサンヒョン <hansh@devfox.co.jp>
2025-08-08 16:34:17 +0900

    (ハン)グループ一覧画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:attendance_client/screens/company_app_admin_home_screen.dart';
 import 'package:attendance_client/screens/company_create_screen.dart';
 import 'package:attendance_client/screens/company_info_screen.dart';
 import 'package:attendance_client/screens/employee_attendance_list_screen.dart';
+import 'package:attendance_client/screens/group_list_screen.dart';
 import 'package:attendance_client/screens/user_create_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';  // login.dartをインポート
@@ -35,6 +36,7 @@ class MyApp extends StatelessWidget {
         '/app-admin/user-list': (context) => const UserListScreen(),
         '/app-admin/user-detail-info': (context) => const UserDetailInfoScreen(),
         '/app-admin/home': (context) => const AppAdminHomeScreen(),
+        '/app-admin/group-list': (context) => const GroupListScreen(),
         '/attend/record': (context) => const AttendanceRecordScreen(),
         '/attend/list': (context) => const EmployeeAttendanceListScreen(),
         '/attend/details': (context) => const UserAttendanceInfoScreen(),
