b8e2bfbeb3de87da613fea804f61031e3988cff9
YONGJUNCHOI-DevfoxYongJun <choiyongjun@devfox.co.jp>
2025-08-14 17:12:05 +0900

    20250814 flushbar add

--- lib/screens/attendance_record_screen.dart
+++ lib/screens/attendance_record_screen.dart
@@ -1,8 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as httpdart;
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:intl/intl.dart';
 
-class AttendanceRecordScreen extends StatelessWidget {
+
+class AttendanceRecordScreen extends StatefulWidget {
   const AttendanceRecordScreen({super.key});
+  @override
+  _AttendanceRecordScreenState createState() => _AttendanceRecordScreenState();
+}
+
+class _AttendanceRecordScreenState extends State<AttendanceRecordScreen> {
+  String? selectedWorkType; // Dropdown 選沢値を保存
+  bool isWorking = false;
+  final storage = const FlutterSecureStorage();
+  String? lastWorkDate; //今日日付
 
   void _userMoreInfo(BuildContext context) {
     Navigator.pushReplacementNamed(
@@ -10,18 +24,40 @@ class AttendanceRecordScreen extends StatelessWidget {
       '/admin/userinfo',
     );
   }
-  void _login(BuildContext context) {
-    Navigator.pushReplacementNamed(
-      context,
-      '/attend/record2',
-    );
+
+  Future<void> sendAttendance(String workType, bool isStart) async {
+    final username = await storage.read(key: "username");
+    final today = DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
+    final url = Uri.parse('http://localhost:8080/attendance'); // Kotlin 서버 주소
+
+    final body = jsonEncode({
+      "username": username,
+      "workType": workType,
+      "isStart": isStart, // true: 출근, false: 퇴근
+      "timestamp": today,
+    });
+
+    try {
+      final response = await httpdart.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        print("출퇴근 정보 전송 성공");
+      } else {
+        print("출퇴근 정보 전송 실패: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("출퇴근 정보 전송 에러: $e");
+    }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     final String formattedDate = DateFormat('yyyy/MM/dd').format(DateTime.now());
+    final usename = storage.read(key: "username");
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -39,7 +75,6 @@ class AttendanceRecordScreen extends StatelessWidget {
                 ),
               ),
             ),
-
             Container(
                 padding: const EdgeInsets.only(bottom: 14),
                 child: Center(
@@ -69,9 +104,9 @@ class AttendanceRecordScreen extends StatelessWidget {
                           ),
 
                           Center(
-                              child: const Text(
-                                '--:--',
-                                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                              child: Text(
+                                '-',
+                                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                               )
                           ),
                         ],
@@ -113,69 +148,124 @@ class AttendanceRecordScreen extends StatelessWidget {
                   color: Colors.black,
                   width: 1,
                 ),
-                // borderRadius: BorderRadius.circular(12),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 14),
-                    child: Text(
-                      'ユーザー氏名',
-                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+              child: FutureBuilder<String?>(
+                future: storage.read(key: "username"),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Text('エラーが発生しました');
+                  } else if (!snapshot.hasData || snapshot.data == null) {
+                    return Text('ユーザーIDが存在しません');
+                  } else {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text( // ログインユーザー名表示
+                          '${snapshot.data}',
+                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 14),
+                        Text(// GPS 情報
+                          'GPS情報（地域名）',
+                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 14),
 
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 14),
-                    child: Text(
-                      'GPS情報（地域名）',
-                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                        // Dropdown BOX--------------------------------------------------------------
+                        DropdownButtonFormField<String?>(
+                          value: selectedWorkType,
+                          decoration: InputDecoration(
+                            labelText: '業務形態',
+                            labelStyle: TextStyle(fontSize: 15, color: Color(0xffcfcfcf)),
+                          ),
+                          hint: const Text('業務形態'),
+                          onChanged: (String? newValue) {
+                            print(newValue);
+                            setState(() {
+                              selectedWorkType = newValue;
+                            });
+                          },
+                          items: ['出社', '在宅']
+                              .map<DropdownMenuItem<String?>>((String? i) {
+                            return DropdownMenuItem<String?>(
+                              value: i,
+                              child: Text(i!),
+                            );
+                          }).toList(),
+                        ),
 
-                  DropdownButtonFormField<String?>(
-                    decoration: InputDecoration(
-                      labelText: '業務形態',
-                      labelStyle: TextStyle(fontSize: 15, color: Color(0xffcfcfcf)),
-                    ),
-                    onChanged: (String? newValue) {
-                      print(newValue);
-                    },
-                    items:
-                    [null, '出社', '在宅'].map<DropdownMenuItem<String?>>((String? i) {
-                      return DropdownMenuItem<String?>(
-                        value: i,
-                        child: Text({'出社': '出社', '在宅': '在宅'}[i] ?? '業務形態'),
-                      );
-                    }).toList(),
-                  ),
-                  const SizedBox(height:15),
-                  ElevatedButton(
-                    onPressed: () => _login(context),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(double.infinity, 60),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      backgroundColor: Colors.indigo,
-                      foregroundColor: Colors.white,
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    child: const Text(
-                      '出勤',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
+                        const SizedBox(height: 15),
 
-                ],
+                        // 出勤退勤ボタン機能
+                        ElevatedButton(
+                          onPressed: () async {
+                            if (selectedWorkType == null) {
+                              Flushbar(
+                                message: '出勤処理に失敗しました。',
+                                duration: Duration(seconds: 1),
+                                flushbarPosition: FlushbarPosition.TOP,
+                                backgroundColor: Colors.red,
+                                margin: EdgeInsets.all(16),
+                                borderRadius: BorderRadius.circular(8),
+                                icon: Icon(Icons.warning, color: Colors.white),
+                              ).show(context);
+                              return;
+                            } else {
+                              final today = DateFormat('yyyy-MM-dd').format(DateTime.now());
+                              final lastWorkDate = await storage.read(key: "lastWorkDate");
+
+                              if (lastWorkDate == today) {
+                                // 今日出勤した場合、退勤だけ可能
+                                if (isWorking) {
+                                  setState(() {
+                                    isWorking = false;
+                                  });
+                                  await storage.write(key: "isWorking", value: "false");
+                                  print("退勤: $selectedWorkType");
+
+                                  // 退勤 POST 呼び出す
+                                  await sendAttendance(selectedWorkType!, false);
+                                } else {
+                                  print("今日はもう出勤できません");
+                                }
+                              } else {
+                                // 今日最初出勤
+                                setState(() {
+                                  isWorking = true;
+                                });
+                                await storage.write(key: "lastWorkDate", value: today);
+                                await storage.write(key: "isWorking", value: "true");
+                                print("出勤: $selectedWorkType");
+
+                                // 出勤 POST 呼び出す
+                                await sendAttendance(selectedWorkType!, true);
+                              }
+                            }
+                          }, style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(double.infinity, 60),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            backgroundColor: isWorking ? Colors.amber : Colors.indigo,
+                            foregroundColor: isWorking ? Colors.black : Colors.white,
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          child: Text(
+                            isWorking ? '退勤' : '出勤',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                },
               ),
-
             ),
-
             Container(
               width: double.infinity,
               padding: const EdgeInsets.all(16),
