56c316394b0191c24e6d62a96703a61e1ad37271
YONGJUNCHOI-DevfoxYongJun <choiyongjun@devfox.co.jp>
2025-08-15 15:04:08 +0900

    250815 git checkout test3

--- lib/screens/attendance_record_screen.dart
+++ lib/screens/attendance_record_screen.dart
@@ -168,7 +168,7 @@ class _AttendanceRecordScreenState extends State<AttendanceRecordScreen> {
                   width: 1,
                 ),
               ),
-              //カスタムビルダー
+              //追加テスト
               child: FutureBuilder<String?>(
                 future: storage.read(key: "username"),
                 builder: (context, snapshot) {
