701815e718b730f18680b4c0f229faaa118fe31a
DevfoxJeongHoeJun <jeonghj@devfox.co.jp>
2025-08-15 13:09:39 +0900

    2025/08/15 update

--- lib/screens/employee_attendance_list_screen.dart
+++ lib/screens/employee_attendance_list_screen.dart
@@ -1,6 +1,125 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-class EmployeeAttendanceListScreen extends StatelessWidget {
+import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class EmployeeAttendanceListScreen extends StatefulWidget {
   const EmployeeAttendanceListScreen({super.key});
+
+  @override
+  State<EmployeeAttendanceListScreen> createState() => _EmpAttandState();
+}
+
+class _EmpAttandState extends State<EmployeeAttendanceListScreen> {
+
+  // LocalStorageを利用するためのライブラリ
+  final storage = new FlutterSecureStorage();
+
+  // 検索・フィルターの入力コントローラー
+  TextEditingController startDateController = TextEditingController();
+  TextEditingController endDateController = TextEditingController();
+  TextEditingController searchController = TextEditingController();
+
+  // リストスクロールコントローラー
+  ScrollController scrollController = ScrollController();
+
+  // ステータス値：勤怠状態「0 = 全体」、「1 = 欠勤」
+  String selectedStatus = '0';
+
+  // 勤怠一覧リスト（ページングデータは append される）
+  List<Map<String, dynamic>> employees = [];
+
+  // ページング初期値
+  int page = 0;   // 現在のページ番号
+  int size = 10;  // 一つのページに出力するデータ数
+
+  //　ローディング状態
+  bool isLoading = false; // 重複ローディング防止「ture = すでにローディング中」
+  bool hasMore = true;    // 次のページの存在可否（サーバー側の Pageable アノテーションで取得可能）
+
+  // 初期状態
+  @override
+  void initState() {
+    super.initState();
+
+    // 初期日時を本日に設定
+    final today = DateTime.now();
+    startDateController.text = DateFormat('yyyy-MM-dd').format(today);
+    endDateController.text = DateFormat('yyyy-MM-dd').format(today);
+
+    // 初期検索キーワードなしに設定
+    searchController.text = '';
+
+    // スクロールリスナー登録
+    scrollController.addListener(_onScroll);
+
+    // 初期ページを呼び出し
+    _handleSearch();
+  }
+
+  // スクロールイベント処理
+  void _onScroll() {
+    // すでにローディング中及び次のページがなければ処理終了
+    if (isLoading || !hasMore) return;
+
+    // 現在のスクロール位置が「最大スクロール長さ - 200」以上なら、次のページを呼び出し
+    final current = scrollController.position.pixels;
+    final max = scrollController.position.maxScrollExtent;
+
+    if (current >= max - 200) {
+      _handleSearch();
+    }
+  }
+
+  // 勤怠一覧データ取得 API を呼び出し
+  // 変数の isRefresh が true　の場合はページ ① から呼び出し（初期表示、検索した場合）
+  // 変数の isRefresh が false の場合は次のページを呼び出し
+  Future<void> _handleSearch({bool isRefresh = false}) async {
+
+    // すてにローディング中なら処理終了
+    if (isLoading) return;
+
+    //　ローディング中の状態に変更
+    setState(() {
+      isLoading = true;
+    });
+
+    if (isRefresh) {
+      page = 0;
+    }
+
+
+    final groupCode = await storage.read(key: "groupCode");
+
+    if (groupCode == null || groupCode.isEmpty) {
+      Navigator.of(context).pushReplacementNamed('/login');
+      return;
+    }
+
+    final url = Uri.parse("http://localhost:8080/api/group/attend-list?page=${page}&size=${size}");
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        "groupCode": groupCode,
+        "startDate": startDateController.text,
+        "endDate": endDateController.text,
+        "status": selectedStatus,
+        "userName": searchController.text
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final decoded = json.decode(response.body) as Map<String, dynamic>;
+      final List<dynamic> bodyList = decoded['body']['content'] ?? [];
+
+      setState(() {
+        employees = bodyList.map((e) => Map<String, dynamic>.from(e)).toList();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,18 +128,33 @@ class EmployeeAttendanceListScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: SingleChildScrollView(
           child: Column(
-            children: const [
-              PageTitle(),
-              SizedBox(height: 16),
-              SearchText(),
-              SizedBox(height: 8),
-              DateRangeInput(),
-              SizedBox(height: 8),
-              StatusSelector(),
-              SizedBox(height: 8),
-              NameSearchInput(),
-              SizedBox(height: 16),
-              EmployeeList(),
+            children: [
+              const PageTitle(),
+              const SizedBox(height: 16),
+              const SearchText(),
+              const SizedBox(height: 8),
+              DateRangeInput(
+                startController: startDateController,
+                endController: endDateController
+              ),
+              const SizedBox(height: 8),
+              StatusSelector(
+                value: selectedStatus,
+                onChanged: (val) => {
+                  setState(() {
+                    selectedStatus = val;
+                  })
+                },
+              ),
+              const SizedBox(height: 8),
+              NameSearchInput(
+                controller: searchController,
+                onSearch: () {
+                  _handleSearch();
+                },
+              ),
+              const SizedBox(height: 16),
+              EmployeeList(employees: employees),
             ],
           ),
         ),
@@ -56,7 +190,14 @@ class SearchText extends StatelessWidget {
 }
 
 class DateRangeInput extends StatefulWidget {
-  const DateRangeInput({super.key});
+  final TextEditingController startController;
+  final TextEditingController endController;
+
+  const DateRangeInput({
+    super.key,
+    required this.startController,
+    required this.endController,
+  });
 
   @override
   State<DateRangeInput> createState() => _DateRangeInputState();
@@ -64,59 +205,43 @@ class DateRangeInput extends StatefulWidget {
 
 //検索日付Widget
 class _DateRangeInputState extends State<DateRangeInput> {
-  DateTime? _startDate;
-  DateTime? _endDate;
 
   Future<void> _selectDate(bool isStart) async {
-    final DateTime initialDate = DateTime.now();
-    final DateTime firstDate = DateTime(2020);
-    final DateTime lastDate = DateTime(2100);
+    final initialDate = DateTime.tryParse(
+      isStart ? widget.startController.text : widget.endController.text,
+    ) ??
+        DateTime.now();
 
-    final DateTime? picked = await showDatePicker(
+    final picked = await showDatePicker(
       context: context,
-      initialDate: isStart
-          ? (_startDate ?? initialDate)
-          : (_endDate ?? initialDate),
-      firstDate: firstDate,
-      lastDate: lastDate,
+      initialDate: initialDate,
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
     );
 
     if (picked != null) {
-      setState(() {
-        if (isStart) {
-          _startDate = picked;
-        } else {
-          _endDate = picked;
-        }
-      });
+      final formatted = '${picked.year}-${picked.month.toString().padLeft(2, '0')}-${picked.day.toString().padLeft(2, '0')}';
+      if (isStart) {
+        widget.startController.text = formatted;
+      } else {
+        widget.endController.text = formatted;
+      }
     }
-  }
-
-  String _formatDate(DateTime? date) {
-    if (date == null) return '';
-    return '${date.year}/${date.month.toString().padLeft(2, '0')}/${date.day.toString().padLeft(2, '0')}';
   }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        // 開始日
         Expanded(
           child: GestureDetector(
             onTap: () => _selectDate(true),
             child: AbsorbPointer(
               child: TextField(
+                controller: widget.startController,
                 decoration: const InputDecoration(
-                  hintText: '2025/07/30',
                   border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 4,
-                    horizontal: 10,
-                  ),
-                ),
-                controller: TextEditingController(
-                  text: _formatDate(_startDate),
+                  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 10),
                 ),
               ),
             ),
@@ -125,22 +250,16 @@ class _DateRangeInputState extends State<DateRangeInput> {
         const SizedBox(width: 8),
         const Text('〜'),
         const SizedBox(width: 8),
-
-        // 終了日
         Expanded(
           child: GestureDetector(
             onTap: () => _selectDate(false),
             child: AbsorbPointer(
               child: TextField(
+                controller: widget.endController,
                 decoration: const InputDecoration(
-                  hintText: '2025/07/30',
                   border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 4,
-                    horizontal: 10,
-                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 10),
                 ),
-                controller: TextEditingController(text: _formatDate(_endDate)),
               ),
             ),
           ),
@@ -150,28 +269,35 @@ class _DateRangeInputState extends State<DateRangeInput> {
   }
 }
 
-class StatusSelector extends StatefulWidget {
-  const StatusSelector({super.key});
+class StatusSelector extends StatelessWidget {
+  final String value;
+  final ValueChanged<String> onChanged;
 
-  @override
-  State<StatusSelector> createState() => _StatusSelectorState();
-}
+  const StatusSelector({
+    super.key,
+    required this.value,
+    required this.onChanged,
+  });
 
-class _StatusSelectorState extends State<StatusSelector> {
-  String selectedStatus = 'すべて';
-  final List<String> statusOptions = ['すべて', '出社', '退勤', '在宅'];
+  final Map<String, String> statusOptions = const {
+    'すべて': '0',
+    '欠勤': '1',
+  };
 
   @override
   Widget build(BuildContext context) {
     return DropdownButtonFormField<String>(
-      value: selectedStatus,
-      items: statusOptions
-          .map((status) => DropdownMenuItem(value: status, child: Text(status)))
+      value: value,
+      items: statusOptions.entries
+          .map((entry) => DropdownMenuItem(
+        value: entry.value,
+        child: Text(entry.key),
+      ))
           .toList(),
-      onChanged: (value) {
-        setState(() {
-          selectedStatus = value!;
-        });
+      onChanged: (newValue) {
+        if (newValue != null) {
+          onChanged(newValue);
+        }
       },
       decoration: const InputDecoration(
         border: OutlineInputBorder(),
@@ -181,8 +307,16 @@ class _StatusSelectorState extends State<StatusSelector> {
   }
 }
 
+
 class NameSearchInput extends StatelessWidget {
-  const NameSearchInput({super.key});
+  final TextEditingController controller;
+  final VoidCallback onSearch;
+
+  const NameSearchInput({
+    super.key,
+    required this.controller,
+    required this.onSearch,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -207,7 +341,7 @@ class NameSearchInput extends StatelessWidget {
         SizedBox(
           height: 48,
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: onSearch,
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.blue,
               foregroundColor: Colors.white,
@@ -230,54 +364,24 @@ class NameSearchInput extends StatelessWidget {
   }
 }
 
-class EmployeeList extends StatelessWidget {
-  const EmployeeList({super.key});
+class EmployeeList extends StatefulWidget {
+  final List<Map<String, dynamic>> employees;
+  const EmployeeList({super.key, required this.employees});
 
   @override
+  State<EmployeeList> createState() => _EmployeeListState();
+}
+
+class _EmployeeListState extends State<EmployeeList> {
+  @override
   Widget build(BuildContext context) {
-    final List<Map<String, String>> employees = [
-      {
-        'mark': 'X',
-        'name': 'キムさん',
-        'home': '在宅',
-        'status1_type': '出勤',
-        'status1_loc': '雑色',
-        'status1_time': '07/30 09:28',
-        'status2_type': '退勤',
-        'status2_loc': '雑色',
-        'status2_time': '07/30 18:35',
-      },
-      {
-        'mark': 'X',
-        'name': 'ソンさん',
-        'home': '在宅',
-        'status1_type': '出勤',
-        'status1_loc': '雑色',
-        'status1_time': '07/30 09:30',
-        'status2_type': '退勤',
-        'status2_loc': '雑色',
-        'status2_time': '07/30 18:33',
-      },
-      {
-        'mark': 'X',
-        'name': 'チョイさん',
-        'home': 'ー',
-        'status1_type': '出勤',
-        'status1_loc': 'ー',
-        'status1_time': '--:--',
-        'status2_type': '退勤',
-        'status2_loc': 'ー',
-        'status2_time': '--:--',
-      },
-    ];
+    final employees = widget.employees;
 
     return Column(
       children: employees.map((employee) {
         return GestureDetector(
           onTap: () {
-            Navigator.of(context).pushNamed(
-              '/attend/details'
-            );
+            Navigator.of(context).pushNamed('/attend/details');
           },
           child: Container(
             margin: const EdgeInsets.only(bottom: 8),
@@ -290,44 +394,61 @@ class EmployeeList extends StatelessWidget {
               children: [
                 Row(
                   children: [
-                    SizedBox(
-                      width: 20,
+                    Expanded(
+                      flex: 3,
                       child: Text(
-                        employee['mark'] ?? '',
+                        employee['userName'] ?? '',
                         style: const TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
                       ),
                     ),
                     Expanded(
                       flex: 2,
                       child: Text(
-                        employee['name'] ?? '',
-                        style: const TextStyle(fontWeight: FontWeight.bold),
+                        '出勤',
+                        textAlign: TextAlign.right,
                       ),
                     ),
                     Expanded(
-                      flex: 1,
-                      child: Text(employee['status1_type'] ?? ''),
-                    ),
-                    Expanded(flex: 1, child: Text(employee['status1_loc'] ?? '')),
-                    Expanded(
                       flex: 2,
-                      child: Text(employee['status1_time'] ?? ''),
+                      child: Text(
+                        employee['startTime'] != null && employee['startTime']!.isNotEmpty
+                            ? DateFormat('MM/dd HH:mm').format(DateTime.parse(employee['startTime']))
+                            : '-- : --',
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ],
                 ),
                 const SizedBox(height: 4),
                 Row(
                   children: [
-                    const SizedBox(width: 20),
-                    const Expanded(flex: 2, child: SizedBox()),
                     Expanded(
-                      flex: 1,
-                      child: Text(employee['status2_type'] ?? ''),
+                      flex: 3,
+                      child: Text(
+                        (employee['type'] == '1')
+                            ? '出社'
+                            : (employee['type'] == '2')
+                            ? '在宅'
+                            : '欠勤',
+                        textAlign: TextAlign.left,
+                      ),
                     ),
-                    Expanded(flex: 1, child: Text(employee['status2_loc'] ?? '')),
                     Expanded(
                       flex: 2,
-                      child: Text(employee['status2_time'] ?? ''),
+                      child: Text(
+                        '退勤',
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        employee['endTime'] != null && employee['endTime']!.isNotEmpty
+                            ? DateFormat('MM/dd HH:mm').format(DateTime.parse(employee['endTime']))
+                            : '-- : --',
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ],
                 ),
@@ -339,3 +460,4 @@ class EmployeeList extends StatelessWidget {
     );
   }
 }
+
