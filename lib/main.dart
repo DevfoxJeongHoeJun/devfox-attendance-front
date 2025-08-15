53ff09b35d8b00c2b9386f1566ef826523105b54
ハンサンヒョン <hansh@devfox.co.jp>
2025-08-15 15:01:05 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: '勤怠管理アプリ',
+      title: '勤怠管理アプリddd',
       initialRoute: initialRoute,
       routes: {
         '/login': (context) => const LoginScreen(),
